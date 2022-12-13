4b81f5d6116a0731ff85aa0ba05b5199fa1f5c06
raspirin <f1193968991@126.com>
2022-12-13 20:11:40 +0800

    sync data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Arg {
+
+    install: Option<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arg::parse();
+
 }
