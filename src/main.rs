7f0e62fb45fcaced5bd810019803d77be6fd97a3
raspirin <f1193968991@126.com>
2022-12-27 16:53:54 +0800

    impl simple downloader

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use clap::{Parser, Subcommand};
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Arg {
-
-    install: Option<String>
-}
+use reqwest::blocking as rq;
 
 fn main() {
-    let args = Arg::parse();
-
+    let client = rq::Client::new();
+    let response = client.get("https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-mac.zip").send().unwrap();
+    let path = std::path::Path::new("bin/ninja-mac.zip");
+    let mut file = std::fs::File::create(path).unwrap();
+    let mut content = std::io::Cursor::new(response.bytes().unwrap());
+    std::io::copy(&mut content, &mut file).unwrap();
 }
