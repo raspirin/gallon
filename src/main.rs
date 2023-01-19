d926fe468eba616fe56941840641a10d548389dd
raspirin <f1193968991@126.com>
2023-01-19 22:37:11 +0800

    impl decompressing by using decompress crate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use decompress::{ExtractOptsBuilder};
 use reqwest::blocking as rq;
 
 fn main() {
+    let res = decompress::decompress(
+        "bin/ninja-mac.zip",
+        "bin",
+        &ExtractOptsBuilder::default().build().unwrap(),
+    )
+    .unwrap();
+}
+
+fn download() {
     let client = rq::Client::new();
-    let response = client.get("https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-mac.zip").send().unwrap();
+    let response = client
+        .get("https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-mac.zip")
+        .send()
+        .unwrap();
     let path = std::path::Path::new("bin/ninja-mac.zip");
     let mut file = std::fs::File::create(path).unwrap();
     let mut content = std::io::Cursor::new(response.bytes().unwrap());
